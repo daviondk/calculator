a1dbb306593e491ebfe1498fec8c44df813b66b8
daviondk <daviondk@yandex.ru>
2019-01-25 18:03:21 +0500

    key constants fixed

--- app/src/main/java/com/example/davio/calc/MainActivity.kt
+++ app/src/main/java/com/example/davio/calc/MainActivity.kt
@@ -1,24 +1,25 @@
 package com.example.davio.calc
 
 import android.content.res.Configuration
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
-import android.widget.Toast
 import com.udojava.evalex.Expression
 import kotlinx.android.synthetic.main.activity_main.*
 import java.math.BigDecimal
 import java.util.*
 
 
-
-
 class MainActivity : AppCompatActivity() {
     val STATE_INPUT = ""
     val STATE_DEQUE_COUNTER: Deque<Int> = LinkedList()
     val STATE_DEQUE_OPERATIONS: Deque<Int> = LinkedList()
-    // -1 - (, 0 - ), 1 - number, 2 - leftfunc, 3 - rightfunc, 4 - simpleOper, 5 - digit after dot
+    var NUMBER_FLAG = 1
+    var LEFT_FUNC_FLAG = 2
+    var RIGHT_FUNC_FLAG = 3
+    var SIMPLE_OPER_FLAG = 4
+    var FRACTION_FLAG = 5
     var memoryFlag = false
     var memoryValue = BigDecimal(0)
     var invert = false
@@ -44,60 +45,62 @@ class MainActivity : AppCompatActivity() {
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
         super.onRestoreInstanceState(savedInstanceState)
-        for(element in savedInstanceState?.getIntArray("stack_operations") as IntArray)
+        for (element in savedInstanceState?.getIntArray("stack_operations") as IntArray)
             STATE_DEQUE_OPERATIONS.addLast(element)
-        for(element in savedInstanceState.getIntArray("stack_counter") as IntArray)
+        for (element in savedInstanceState.getIntArray("stack_counter") as IntArray)
             STATE_DEQUE_COUNTER.addLast(element)
         input_field.text = savedInstanceState.getString(STATE_INPUT)
         memoryFlag = savedInstanceState.getBoolean("memFlag")
         lastNumberWithDotFlag = savedInstanceState.getBoolean("dot")
         invert = savedInstanceState.getBoolean("inv")
-        if(invert && getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        if (invert && getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             switchInvert()
         }
         unit = savedInstanceState.getBoolean("unit")
-        if(unit && getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        if (unit && getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             switchUnit()
         }
         memoryValue = BigDecimal(savedInstanceState.getString("memValue"))
         getResult()
     }
+
     fun append(textView: TextView, appendedText: String) {
         textView.text = textView.text.toString() + appendedText
     }
 
     fun appendFront(textView: TextView, appendedText: String) {
-        textView.text =  appendedText + textView.text.toString()
+        textView.text = appendedText + textView.text.toString()
     }
 
     fun onCharacterClick(view: View) {
         val curText = (view as? TextView)?.contentDescription.toString()
         onCharacterClickString(curText)
     }
-    fun onCharacterClickString(curText : String) {
+
+    fun onCharacterClickString(curText: String) {
         append(input_field, curText)
         STATE_DEQUE_COUNTER.addFirst(curText.length)
-        if(!lastNumberWithDotFlag) {
-            STATE_DEQUE_OPERATIONS.addFirst(1)
+        if (!lastNumberWithDotFlag) {
+            STATE_DEQUE_OPERATIONS.addFirst(NUMBER_FLAG)
         } else {
-            STATE_DEQUE_OPERATIONS.addFirst(5)
+            STATE_DEQUE_OPERATIONS.addFirst(FRACTION_FLAG)
         }
         getResult()
     }
 
     fun onSimpleOperationClick(view: View) {
-        if((STATE_DEQUE_OPERATIONS.isEmpty() || STATE_DEQUE_OPERATIONS.peek() == 2 || STATE_DEQUE_OPERATIONS.peek() == -1) && view.id == minus.id
-                || STATE_DEQUE_OPERATIONS.peek() == 5 || STATE_DEQUE_OPERATIONS.peek() == 3
-                || STATE_DEQUE_OPERATIONS.peek() == 1  || STATE_DEQUE_OPERATIONS.peek() == 0) {
+        if ((STATE_DEQUE_OPERATIONS.isEmpty() || STATE_DEQUE_OPERATIONS.peek() == LEFT_FUNC_FLAG || STATE_DEQUE_OPERATIONS.peek() == -NUMBER_FLAG) && view.id == minus.id
+                || STATE_DEQUE_OPERATIONS.peek() == FRACTION_FLAG || STATE_DEQUE_OPERATIONS.peek() == RIGHT_FUNC_FLAG
+                || STATE_DEQUE_OPERATIONS.peek() == NUMBER_FLAG || STATE_DEQUE_OPERATIONS.peek() == 0) {
             onCharacterClick(view)
             STATE_DEQUE_OPERATIONS.pop()
-            STATE_DEQUE_OPERATIONS.addFirst(4)
-        } else if(STATE_DEQUE_OPERATIONS.peek() == 4) {
+            STATE_DEQUE_OPERATIONS.addFirst(SIMPLE_OPER_FLAG)
+        } else if (STATE_DEQUE_OPERATIONS.peek() == SIMPLE_OPER_FLAG) {
             onBackspaceClick(backspace)
 
             onCharacterClick(view)
             STATE_DEQUE_OPERATIONS.pop()
-            STATE_DEQUE_OPERATIONS.addFirst(4)
+            STATE_DEQUE_OPERATIONS.addFirst(SIMPLE_OPER_FLAG)
         }
         lastNumberWithDotFlag = false
         //getResult()
@@ -106,46 +109,49 @@ class MainActivity : AppCompatActivity() {
     fun onLeftFunctionClick(view: View) {
         val curText = (view as? TextView)?.contentDescription.toString()
         /*if(input_field.text.isEmpty() || !operationFlag){
-            curText.padStart(1, '*')
+            curText.padStart(NUMBER_FLAG, '*')
         }*/
         append(input_field, curText)
         STATE_DEQUE_COUNTER.addFirst(curText.length)
-        STATE_DEQUE_OPERATIONS.addFirst(2)
+        STATE_DEQUE_OPERATIONS.addFirst(LEFT_FUNC_FLAG)
         lastNumberWithDotFlag = false
         getResult()
     }
+
     fun onRightFunctionClick(view: View) {
-        if(STATE_DEQUE_OPERATIONS.peek() == 0 || STATE_DEQUE_OPERATIONS.peek() == 5 ||
-                STATE_DEQUE_OPERATIONS.peek() == 1 || STATE_DEQUE_OPERATIONS.peek() == 3) {
+        if (STATE_DEQUE_OPERATIONS.peek() == 0 || STATE_DEQUE_OPERATIONS.peek() == FRACTION_FLAG ||
+                STATE_DEQUE_OPERATIONS.peek() == NUMBER_FLAG || STATE_DEQUE_OPERATIONS.peek() == RIGHT_FUNC_FLAG) {
             onLeftFunctionClick(view)
             STATE_DEQUE_OPERATIONS.pop()
-            STATE_DEQUE_OPERATIONS.addFirst(3)
+            STATE_DEQUE_OPERATIONS.addFirst(RIGHT_FUNC_FLAG)
         }
         lastNumberWithDotFlag = false
     }
 
     fun onBackspaceClick(view: View) {
-        if(!STATE_DEQUE_COUNTER.isEmpty()){
+        if (!STATE_DEQUE_COUNTER.isEmpty()) {
             input_field.text = input_field.text.dropLast(STATE_DEQUE_COUNTER.pop())
             STATE_DEQUE_OPERATIONS.pop()
             getResult()
         }
     }
+
     fun onClearClick(view: View) {
         input_field.text = ""
         result_field.text = ""
         STATE_DEQUE_COUNTER.clear()
         STATE_DEQUE_OPERATIONS.clear()
     }
+
     fun onPlusMinusClick(view: View) {
-        if(input_field.text.isEmpty()){
+        if (input_field.text.isEmpty()) {
             append(input_field, "-")
-            STATE_DEQUE_COUNTER.addFirst(1)
-            STATE_DEQUE_OPERATIONS.addFirst(4)
+            STATE_DEQUE_COUNTER.addFirst(NUMBER_FLAG)
+            STATE_DEQUE_OPERATIONS.addFirst(SIMPLE_OPER_FLAG)
         } else {
             appendFront(input_field, "-(")
-            STATE_DEQUE_COUNTER.addLast(2)
-            STATE_DEQUE_OPERATIONS.addLast(4)
+            STATE_DEQUE_COUNTER.addLast(LEFT_FUNC_FLAG)
+            STATE_DEQUE_OPERATIONS.addLast(SIMPLE_OPER_FLAG)
         }
         getResult()
     }
@@ -154,22 +160,24 @@ class MainActivity : AppCompatActivity() {
         memoryFlag = false
         memoryValue = BigDecimal.ZERO
     }
+
     fun onMRClick(view: View) {
-        if(STATE_DEQUE_OPERATIONS.peek() != 1 && STATE_DEQUE_OPERATIONS.peek() != 5 && memoryFlag) {
+        if (STATE_DEQUE_OPERATIONS.peek() != NUMBER_FLAG && STATE_DEQUE_OPERATIONS.peek() != FRACTION_FLAG && memoryFlag) {
             lastNumberWithDotFlag = false
-            for(digit in memoryValue.toString()) {
-                if(digit == ',') {
+            for (digit in memoryValue.toString()) {
+                if (digit == ',') {
                     lastNumberWithDotFlag = true
                 }
                 append(input_field, digit.toString())
-                STATE_DEQUE_COUNTER.addFirst(1)
-                STATE_DEQUE_OPERATIONS.addFirst(1)
+                STATE_DEQUE_COUNTER.addFirst(NUMBER_FLAG)
+                STATE_DEQUE_OPERATIONS.addFirst(NUMBER_FLAG)
             }
         }
     }
+
     fun onMPlusClick(view: View) {
         getResult()
-        if(!result_field.text.isEmpty() && result_field.text != "Error") {
+        if (!result_field.text.isEmpty() && result_field.text != "Error") {
             memoryFlag = true
             memoryValue = memoryValue.add(BigDecimal(result_field.text.toString()))
         } else {
@@ -177,9 +185,10 @@ class MainActivity : AppCompatActivity() {
             memoryValue = BigDecimal.ZERO
         }
     }
+
     fun onMMinusClick(view: View) {
         getResult()
-        if(!result_field.text.isEmpty() && result_field.text != "Error") {
+        if (!result_field.text.isEmpty() && result_field.text != "Error") {
             memoryFlag = true
             memoryValue = memoryValue.subtract(BigDecimal(result_field.text.toString()))
         } else {
@@ -187,6 +196,7 @@ class MainActivity : AppCompatActivity() {
             memoryValue = BigDecimal.ZERO
         }
     }
+
     fun getResult() {
         val expr_string = input_field.text.toString()
         var balance = 0
@@ -209,32 +219,36 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     fun onEqualityClick(view: View) {
         getResult()
-        if(!result_field.text.isEmpty() && result_field.text != "Error") {
+        if (!result_field.text.isEmpty() && result_field.text != "Error") {
             input_field.text = ""
             STATE_DEQUE_COUNTER.clear()
             STATE_DEQUE_OPERATIONS.clear()
 
-            for(symbol in result_field.text) {
+            for (symbol in result_field.text) {
                 onCharacterClickString(symbol.toString())
             }
         }
     }
+
     fun onOpenBracketClick(view: View) {
         onCharacterClick(view)
         STATE_DEQUE_OPERATIONS.pop()
-        STATE_DEQUE_OPERATIONS.addFirst(-1)
+        STATE_DEQUE_OPERATIONS.addFirst(-NUMBER_FLAG)
 
     }
+
     fun onCloseBracketClick(view: View) {
         onCharacterClick(view)
         STATE_DEQUE_OPERATIONS.pop()
         STATE_DEQUE_OPERATIONS.addFirst(0)
     }
-    fun switchInvert(){
-        if(!invert) {
-            if(!unit) {
+
+    fun switchInvert() {
+        if (!invert) {
+            if (!unit) {
                 sin.text = "sin"
                 cos.text = "cos"
                 tan.text = "tan"
@@ -243,9 +257,8 @@ class MainActivity : AppCompatActivity() {
                 cos.text = "cosh"
                 tan.text = "tanh"
             }
-        }
-        else {
-            if(!unit) {
+        } else {
+            if (!unit) {
                 sin.text = "asin"
                 cos.text = "acos"
                 tan.text = "atan"
@@ -255,29 +268,32 @@ class MainActivity : AppCompatActivity() {
                 tan.text = "atanh"
             }
         }
-        sin.contentDescription = sin.text.toString()+"("
-        cos.contentDescription = cos.text.toString()+"("
-        tan.contentDescription = tan.text.toString()+"("
+        sin.contentDescription = sin.text.toString() + "("
+        cos.contentDescription = cos.text.toString() + "("
+        tan.contentDescription = tan.text.toString() + "("
     }
+
     fun onInvertClick(view: View) {
         invert = invert.not()
         switchInvert()
     }
+
     fun onPointClick(view: View) {
-        if(!lastNumberWithDotFlag && (STATE_DEQUE_OPERATIONS.isEmpty() || STATE_DEQUE_OPERATIONS.peek() == 1)) {
+        if (!lastNumberWithDotFlag && (STATE_DEQUE_OPERATIONS.isEmpty() || STATE_DEQUE_OPERATIONS.peek() == NUMBER_FLAG)) {
             lastNumberWithDotFlag = true
             onCharacterClick(view)
         }
     }
-    fun switchUnit(){
-        if(unit) {
+
+    fun switchUnit() {
+        if (unit) {
             angle_unit.text = "DEG"
-        }
-        else {
+        } else {
             angle_unit.text = "RAD"
         }
         switchInvert()
     }
+
     fun onSwitchUnitClick(view: View) {
         unit = unit.not()
         switchUnit()
